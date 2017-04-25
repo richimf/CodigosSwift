1fcefe39ea2c96bca29bc0017433428ac0050643
Richie <ricardo.montesinos@MXMCP92UEDTY3.local>
2017-04-25 17:35:10 -0500

    Customizing Steppers, Sliders, and Switches

--- UIApperanceTutorial/Pet Finder_Starter_Swift3/Pet Finder/Theme.swift
+++ UIApperanceTutorial/Pet Finder_Starter_Swift3/Pet Finder/Theme.swift
@@ -96,6 +96,28 @@ enum Theme: Int {
     UISegmentedControl.appearance().setBackgroundImage(controlSelectedBackground,
                                                        for: .selected,
                                                        barMetrics: .default)
+    
+    //7 Stepper control [+|-]
+    UIStepper.appearance().setBackgroundImage(controlBackground, for: .normal)
+    UIStepper.appearance().setBackgroundImage(controlBackground, for: .disabled)
+    UIStepper.appearance().setBackgroundImage(controlBackground, for: .highlighted)
+    UIStepper.appearance().setDecrementImage(UIImage(named: "fewerPaws"), for: .normal)
+    UIStepper.appearance().setIncrementImage(UIImage(named: "morePaws"), for: .normal)
+    
+    
+    //8 Slider
+    UISlider.appearance().setThumbImage(UIImage(named: "sliderThumb"), for: .normal)
+    UISlider.appearance().setMaximumTrackImage(UIImage(named: "maximumTrack")?
+      .resizableImage(withCapInsets:UIEdgeInsets(top: 0, left: 0.0, bottom: 0, right: 6.0)), for: .normal)
+    
+    UISlider.appearance().setMinimumTrackImage(UIImage(named: "minimumTrack")?
+      .withRenderingMode(.alwaysTemplate)
+      .resizableImage(withCapInsets:UIEdgeInsets(top: 0, left: 6.0, bottom: 0, right: 0)), for: .normal)
+    
+    //9 Switch
+    UISwitch.appearance().onTintColor = mainColor.withAlphaComponent(0.3)
+    UISwitch.appearance().thumbTintColor = mainColor
+    
   }
   
   //Customizing the Tab Bar
