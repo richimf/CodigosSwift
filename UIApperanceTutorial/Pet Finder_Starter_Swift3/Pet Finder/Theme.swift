88ba36b1c2922fd51608da8617916253449c0ff4
Richie <ricardo.montesinos@MXMCP92UEDTY3.local>
2017-04-25 16:11:14 -0500

    TabBar customization

--- UIApperanceTutorial/Pet Finder_Starter_Swift3/Pet Finder/Theme.swift
+++ UIApperanceTutorial/Pet Finder_Starter_Swift3/Pet Finder/Theme.swift
@@ -64,7 +64,19 @@ enum Theme: Int {
     //Here you’re simply setting the image and transition mask image to be used as the back indicator.
     UINavigationBar.appearance().backIndicatorImage = UIImage(named: "backArrow")
     UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "backArrowMaskFixed")
-
+    
+    //5 Customize Tab Bar
+    UITabBar.appearance().barStyle = barStyle
+    UITabBar.appearance().backgroundImage = tabBarBackgroundImage
+    
+    let tabIndicator = UIImage(named: "tabBarSelectionIndicator")?.withRenderingMode(.alwaysTemplate)
+    let tabResizableIndicator = tabIndicator?.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 2.0, bottom: 0, right: 2.0))
+    UITabBar.appearance().selectionIndicatorImage = tabResizableIndicator
+  }
+  
+  //Customizing the Tab Bar
+  var tabBarBackgroundImage: UIImage? {
+    return self == .graphical ? UIImage(named: "tabBarBackground") : nil
   }
   
 }
