617c1b900c196a988abd9c692782cb83a175c760
Richie <richimf1@gmail.com>
2017-12-12 23:00:08 -0600

    Functions basics

---
+++ Swift Basics/Functions.playground/Contents.swift
@@ -0,0 +1,75 @@
+/*import UIKit
+ The UIKit framework provides the required infrastructure for your iOS or tvOS apps.
+ It provides the window and view architecture for implementing your interface, the event handling infrastructure for delivering Multitouch
+ and other types of input to your app, and the main run loop needed to manage interactions among the user, the system, and your app. Other
+ features offered by the framework include animation support, document support, drawing and printing support, information about the current
+ device, text management and display, search support, accessibility support, app extension support, and resource management.
+ */
+
+//*----------------------| SWIFT FUNCTIONS |----------------------*//
+
+//-----------> BASICS <-----------//
+//Swift copies the value before passing it oto the function, a behavior known as pass-by-value
+// Ideally a function doesn't alter its parameters.
+func printHola(){
+    print("hola")
+}
+
+func printHola(v:String){
+    print(v)
+}
+
+func printHola(_ v:String){
+    print(v)
+}
+
+func printHola(v:String, i:Int){
+    print("\(v) \(i)")
+}
+
+func printTwoThings(name:String, and lastName:String){
+    print("\(name) \(lastName)")
+}
+
+func multiply(number:Int, by number2:Int)->Int{
+    return number * number2
+}
+
+func multiplyAndDivide(_ number: Int, by factor: Int) -> (product: Int, quotient: Int) {
+    return (number * factor, number / factor)
+}
+let results = multiplyAndDivide(4, by: 2)
+let product = results.product
+let quotient = results.quotient
+
+printHola()
+printHola(v: "hola que hace")
+printHola("hola que hace")
+printHola(v: "hola", i: 6)
+printTwoThings(name: "richie", and: "mont")
+multiply(number: 8, by: 5)
+
+
+//-----------> INOUT <-----------//
+
+/*The inout keyword before the parameter type indicates that this parameter should be copied in,
+ that local copy used within the function, and then copied back out when the function returns.*/
+func increment(value:inout Int){
+    value += 1
+    print(value)
+}
+
+var val = 5 //como si fuera por referencia
+increment(value: &val)
+print(val)
+
+
+
+
+
+
+
+
+
+
+
