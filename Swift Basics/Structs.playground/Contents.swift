48cc0ce07d40302026d0401c661371660dd9d386
Richie <richimf1@gmail.com>
2017-12-14 22:27:16 -0600

    update code struct

--- Swift Basics/Structs.playground/Contents.swift
+++ Swift Basics/Structs.playground/Contents.swift
@@ -135,6 +135,30 @@ var mihola = Hola(valor: 6)
 mihola.valor = 4
 mihola.valor = 8
 
+//Class example:
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+// About to set totalSteps to 200
+// Added 200 steps
+stepCounter.totalSteps = 360
+// About to set totalSteps to 360
+// Added 160 steps
+stepCounter.totalSteps = 896
+// About to set totalSteps to 896
+// Added 536 steps
+
 struct Level {
     static var highestLevel = 1 //static stores the value into memory
     let id: Int
